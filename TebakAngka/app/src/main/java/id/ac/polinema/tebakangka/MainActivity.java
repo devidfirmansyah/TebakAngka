716b7a22001038a223bd8bfe8b4c0a9fa3472230
Devid <firmansyahdevid@gmail.com>
2019-09-12 17:10:27 +0700

    Commit changes of activity_simple.xml, AndroidManifestxml, MainActivity.java, SimpleActivity.java and string.xml

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -12,25 +12,53 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	//  TODO: deklarasikan variabel di sini
+    //  TODO: deklarasikan variabel di sini
+    private EditText etAngka;
+    int angkaRandom = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
-	}
 
-	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO: bind layout di sini
+        etAngka = (EditText) findViewById(R.id.number_input);
+    }
 
-	}
+    // TODO: generate angka random di sini
+    private void initRandomNumber() {
+        Random Angka = new Random();
+        if(angkaRandom == 0){
+            angkaRandom = Angka.nextInt(100);
+        }
+        else{
+            angkaRandom = angkaRandom;
+        }
+    }
 
-	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
-	}
+    public void handleGuess(View view) {
+        initRandomNumber();
+        // TODO: Tambahkan logika untuk melakukan pengecekan angka
+        if (etAngka.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "Masukkan Angka!!!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (angkaRandom < Integer.parseInt(etAngka.getText().toString())) {
+                Toast.makeText(this, "Tebakan Anda Lebih Besar", Toast.LENGTH_SHORT).show();
+                etAngka.setText("");
+            } else if (angkaRandom > Integer.parseInt(etAngka.getText().toString())) {
+                Toast.makeText(this, "Tebakan Anda Lebih Kecil", Toast.LENGTH_SHORT).show();
+                etAngka.setText("");
+            } else {
+                Toast.makeText(this, "Tebakan Anda Benar", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
-	public void handleReset(View view) {
-		// TODO: Reset tampilan
-	}
+    public void handleReset(View view) {
+        // TODO: Reset tampilan
+        angkaRandom = 0;
+        initRandomNumber();
+        etAngka.setText("");
+        Toast.makeText(this, "Reset Berhasil", Toast.LENGTH_SHORT).show();
+    }
 }
